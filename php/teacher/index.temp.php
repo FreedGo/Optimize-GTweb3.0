f39e75520d16e983759fca7410a1a1bd6254fccf
Freed <flyxz@126.com>
2016-11-07 17:15:28 +0800

    老师前台页面增加课表功能显示

--- php/teacher/index.temp.php
+++ php/teacher/index.temp.php
@@ -85,10 +85,11 @@ if ($dangid != 0) {
                 <a href="<?= $public_r['newsurl'] ?>e/space/?userid=<?= $userid ?>">
                     <div class="touxiang"><img src="<?= $userpic ?>"></div>
                 </a>
-                <div class="uesrID"><?= $username ?>
+                <div class="uesrID">
+                    <h2 class="username"><?= $username ?></h2>
                     <?
                     if ($cked == 1) {
-                        echo "<i class='iconfont hongse'>&#xe657;</i>";
+                        echo "<i class='iconfont newRenZheng'></i>";
                     }
                     ?>
                 </div>
@@ -296,6 +297,7 @@ if ($dangid != 0) {
                                 <li><a href="javascript:;" target="_self">课程中心</a><span></span></li>
                                 <li><a href="javascript:;">老师动态</a><span></span></li>
                                 <li><a href="javascript:;" target="_self">在线直播</a><span></span></li>
+                                <li class="showAlert"><a href="javascript:;" target="_self">课表</a><span></span></li>
                                 <li class="showAlert"><a href="javascript:;" target="_self">全部学员</a><span></span></li>
                                 <li class="discuss-area-btn showAlert"><a href="javascript:;" target="_self">讨论区</a><span></span></li>
                             </ul>
@@ -441,6 +443,191 @@ if ($dangid != 0) {
 <!--                                    </div>-->
 <!--                                </li>-->
                             </ul>
+                            <!-- 课表·································································· -->
+                            <?php
+                            if ($zjj == 11) {
+                                ?>
+                                <ul class="liebiaoFuck liebiaoShow current kebiao clearfix">
+                                    <?php
+                                    //课表数据处理
+                                    $num=$empire->num("select id from phome_zjk_kebiao_tear where tearid=$userid");
+                                    if($num>0){
+                                        //本月课表数据
+                                        $sql=$empire->query("select * from phome_zjk_kebiao_tear where tearid=$userid  and date_format(classtime,'%Y-%m')=date_format(now(),'%Y-%m')");
+                                        while($r=$empire->fetch($sql))
+                                        {
+                                            $kebiao.="{id:'$r[id]',title: '$r[couname]',start: '$r[classtime] $r[starttime]',end:'$r[classtime] $r[stoptime]',addres:'$r[location]',student:'$r[stuname]',status:'$r[retype]',mark:'$r[couname]',allDay:false,backgroundColor:'#90F79B',borderColor:'#ff0',textColor:'#000'},";
+                                        }
+                                        //$biao=substr($kebiao, 0, -1);
+                                        //echo $biao;
+                                    }
+                                    ?>
+
+                                    <!--	课表依赖-->
+                                    <link href='/e/data/html/kebiao/lib/fullcalendar.min.css' rel='stylesheet' />
+                                    <script src='/e/data/html/kebiao/lib/moment.min.js'></script>
+                                    <script src='/e/data/html/kebiao/lib/fullcalendar.min.js'></script>
+                                    <script src="/e/data/html/kebiao/lib/zh-cn.js"></script>
+                                    <script>
+                                        $(document).ready(function() {
+                                            $('#calendar').fullCalendar({
+                                                theme: false,
+                                                header: {
+                                                    left: 'prev,next today',
+                                                    center: 'title',
+                                                    right: 'month,agendaWeek,agendaDay'
+                                                },
+//					isRTL:true,//本来用这个参数来控制title与time显示顺序,但是会影响别的东西
+                                                buttonText:{
+
+                                                    today: '转到今日',
+                                                    month: '月视图',
+                                                    week: '周视图',
+                                                    day: '日视图'
+                                                },
+//			defaultDate: '2016-09-12',
+                                                navLinks: true, // can click day/week names to navigate views
+                                                editable: true,
+                                                //月视图下日历格子宽度和高度的比例
+                                                aspectRatio: 1.35,
+                                                //月视图的显示模式，fixed：固定显示6周高；liquid：高度随周数变化；variable: 高度固定
+                                                weekMode: 'liquid',
+                                                //初始化时的默认视图，month、agendaWeek、agendaDay
+                                                defaultView: 'month',
+                                                //agenda视图下是否显示all-day
+                                                allDaySlot: true,
+                                                //agenda视图下all-day的显示文本
+                                                allDayText: '全天',
+                                                //agenda视图下两个相邻时间之间的间隔
+                                                slotMinutes: 30,
+                                                //区分工作时间
+                                                businessHours: true,
+                                                //非all-day时，如果没有指定结束时间，默认执行120分钟
+                                                defaultEventMinutes: 120,
+                                                //设置为true时，如果数据过多超过日历格子显示的高度时，多出去的数据不会将格子挤开，而是显示为 +...more ，点击后才会完整显示所有的数据
+                                                firstDay:0,
+                                                eventLimit: true,
+                                                dayClick: function(date, allDay, jsEvent, view) {
+                                                    //单击日历中的某一天时
+//						console.log(date);
+//						console.log(allDay);
+//						console.log(jsEvent);
+//						console.log(view);
+                                                },
+                                                eventClick: function(calEvent, jsEvent, view) {
+                                                    //日历中的某一日程（事件）时，触发此操作
+//						console.log(calEvent);
+//						console.log(jsEvent);
+//						console.log(view);
+                                                    $('.itemCon1').empty().html(calEvent.title);//标题
+                                                    $('.itemCon2').empty().html(calEvent.title);//地址
+                                                    $('.itemCon3').empty().html(calEvent.start._i);//开始
+                                                    $('.itemCon4').empty().html(calEvent.end._i);//结束
+                                                    $('.itemCon5').empty().html(calEvent.student);//学生
+                                                    $('.itemCon6').empty().html(calEvent.status);//课程状态
+                                                    $('.itemCon7').empty().html(calEvent.mark);//备注
+
+                                                    $('.single-kecheng').show();
+
+
+                                                },
+
+                                                events: [<?=$kebiao?>],
+
+                                            });
+
+                                        });
+
+                                    </script>
+                                    <!--end   课表依赖-->
+                                    <!--		日期控件依赖-->
+                                    <link rel="stylesheet" type="text/css" href="/e/data/html/kebiao/lib/lq.datetimepick.css"/>
+                                    <script src='/e/data/html/kebiao/lib/selectUi.js' type='text/javascript'></script>
+                                    <script src='/e/data/html/kebiao/lib/lq.datetimepick.js' type='text/javascript'></script>
+                                    <script src="/e/data/html/kebiao/lib/kebiao.js"></script>
+                                    <script type="text/javascript">
+                                        $(function () {
+                                            $("#datetimepicker1").on("click",function(e){
+                                                e.stopPropagation();
+                                                $(this).lqdatetimepicker({
+                                                    css : 'datetime-hour'
+                                                });
+                                            });
+                                            $("#datetimepicker2").on("click",function(e){
+                                                e.stopPropagation();
+                                                $(this).lqdatetimepicker({
+                                                    css : 'datetime-hour'
+                                                });
+                                            });
+                                            $("#datetimepicker3").on("click",function(e){
+                                                e.stopPropagation();
+                                                $(this).lqdatetimepicker({
+                                                    css : 'datetime-day',
+                                                    dateType : 'D',
+                                                    selectback : function(){
+                                                    }
+                                                });
+                                            });
+                                        });
+                                        /*字数限制100个*/
+                                        $(function () {
+                                            $("#textInput").on("input propertychange", function() {
+                                                var $this = $(this),
+                                                    _val = $this.val(),
+                                                    count = "";
+                                                if (_val.length > 100) {
+                                                    $this.val(_val.substring(0, 100));
+                                                }
+                                                count = 100 - $this.val().length;
+                                                $("#text-count").text(count);
+                                            });
+                                        });
+                                    </script>
+                                    <!--end日期控件依赖-->
+                                    <div id='calendar'></div>
+                                    <div class="single-kecheng">
+                                        <i class="shutUp">×</i>
+                                        <div class="single-ke-con">
+                                            <div class="single-ke-item clearfix">
+                                                <h4 class="ke-itme-tips">课程名称：</h4>
+                                                <p class="ke-item-con itemCon1">XXXXXXXXXXXX</p>
+                                            </div>
+                                            <div class="single-ke-item clearfix">
+                                                <h4 class="ke-itme-tips">上课地点：</h4>
+                                                <p class="ke-item-con itemCon2">XXXXXXXXXXXX</p>
+                                            </div>
+                                            <div class="single-ke-item clearfix">
+                                                <h4 class="ke-itme-tips">开始时间：</h4>
+                                                <p class="ke-item-con itemCon3">XXXXXXXXXXXX</p>
+                                            </div>
+                                            <div class="single-ke-item clearfix">
+                                                <h4 class="ke-itme-tips">结束时间：</h4>
+                                                <p class="ke-item-con itemCon4">XXXXXXXXXXXX</p>
+                                            </div>
+                                            <div class="single-ke-item clearfix">
+                                                <h4 class="ke-itme-tips">上课学生：</h4>
+                                                <p class="ke-item-con itemCon5">XXXXXXXXXXXX</p>
+                                            </div>
+                                            <div class="single-ke-item clearfix">
+                                                <h4 class="ke-itme-tips">课程状态：</h4>
+                                                <p class="ke-item-con itemCon6">XXXXXXXXXXXX</p>
+                                            </div>
+                                            <div class="single-ke-item clearfix">
+                                                <h4 class="ke-itme-tips">课程备注：</h4>
+                                                <p class="ke-item-con itemCon7">XXXXXXXXXXXXXXXXXX
+                                                    XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+                                                    XXXXX</p>
+                                            </div>
+                                            <!--<div class="single-ke-item clearfix">-->
+                                            <!--<button class="single-ke-btn edi-kecheng">修改课程</button>-->
+                                            <!--<button class="single-ke-btn del-kecheng">删除课程</button>-->
+                                            <!--</div>-->
+                                        </div>
+                                    </div>
+                                </ul>
+                                <?
+                                }
+                            ?>
                             <!-- 全部学员··············································································-->
                             <?php
                                 if ($zjj == 11) {
