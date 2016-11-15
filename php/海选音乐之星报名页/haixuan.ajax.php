e2ae0dca15e6fdc68da5520ec624d816e4ad8b8e
Freed <flyxz@126.com>
2016-11-15 14:07:15 +0800

    音乐之星报名修改备份

---
+++ php/海选音乐之星报名页/haixuan.ajax.php
@@ -0,0 +1,19 @@
+<?php
+require('../../e/class/connect.php'); //引入数据库配置文件和公共函数文件 
+require('../../e/class/db_sql.php'); //引入数据库操作文件 
+$link=db_connect(); //连接MYSQL 
+$empire=new mysqlquery(); //声明数据库操作类
+
+$titleid=$_POST['titleid'];
+if(empty($titleid)){
+	exit;
+}
+
+$sql=$empire->query("select sitename,renminbi from {$dbtbpre}enewsbe where titleid='$titleid'");
+while($r=$empire->fetch($sql))
+{
+?>
+<option value="<?=$r[sitename]?>"><?=$r[sitename]?><?=$r[renminbi]?></option>
+<?
+}	
+?>
