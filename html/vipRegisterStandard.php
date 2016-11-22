1804700eeafa8f5454f9ea46e65cc68ddfc18d7d
Freed <flyxz@126.com>
2016-11-22 14:39:31 +0800

    琴行注册标准页面

---
+++ html/vipRegisterStandard.php
@@ -0,0 +1,425 @@
+<!DOCTYPE html>
+<html lang="zh-CN">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport"
+	      content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<!-- 新 Bootstrap 核心 CSS 文件 -->
+	<link rel="stylesheet" href="http://cdn.bootcss.com/bootstrap/3.3.0/css/bootstrap.min.css">
+	<title>Title</title>
+	<style>
+		.f-l-l{
+			float: left;
+		}
+		.qinhang-add{
+			display: inline-block;
+			margin-bottom: 20px;
+		}
+		#address{
+			display: block;
+			width:100%;
+		}
+		.table{
+			background-color: #f7f7f7;
+		}
+		@media (min-width: 768px){
+			.form-inline .form-control{
+				width: 100%;
+			}
+		}
+
+	</style>
+	<!-- jQuery文件。务必在bootstrap.min.js 之前引入 -->
+	<script src="http://cdn.bootcss.com/jquery/1.11.1/jquery.min.js"></script>
+
+	<!-- 最新的 Bootstrap 核心 JavaScript 文件 -->
+	<script src="http://cdn.bootcss.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+	<script src="https://unpkg.com/vue/dist/vue.js"></script>
+</head>
+<body>
+<div class="container" id="qhMsg">
+	<h1 class="pageName text-center">琴行注册标准</h1>
+	<form action="" class="form-inline" role="form" method="post">
+		<div class="table-warp essential-info table-responsive">
+			<h2>一.基本信息</h2>
+			<div class="row qinhang-add">
+				<div class="col-sm-4">
+					<div class="input-group">
+						<input type="text" v-model="addresShi" name="addresShi" class="form-control">
+						<span class="input-group-btn">
+				        <button class="btn btn-default" type="button">市</button>
+				      </span>
+					</div>
+				</div>
+				<div class="col-sm-4">
+					<div class="input-group">
+						<input type="text" v-model="addresQu" class="form-control">
+						<span class="input-group-btn">
+				        <button class="btn btn-default" type="button">区(县)</button>
+				      </span>
+					</div>
+				</div>
+				<div class="col-sm-4">
+					<div class="input-group">
+						<input type="text" v-model="qhTitle" class="form-control">
+						<span class="input-group-btn">
+				        <button class="btn btn-default" type="button">琴行资料表</button>
+				      </span>
+					</div>
+				</div>
+			</div>
+
+			<table class="table  table-bordered">
+					<tr>
+						<td>客户名称</td>
+						<td colspan="2">
+							<div class="form-group">
+								<input type="text" class="form-control" v-model="adminName" id="adminName" required>
+							</div>
+						</td>
+						<td>公司电话</td>
+						<td colspan="2">
+							<div class="form-group">
+								<input type="text" class="form-control" v-model="phoneNumber" id="phoneNumber" required>
+							</div>
+						</td>
+					</tr>
+					<tr>
+						<td>琴行类型</td>
+						<td colspan="5">
+							<label class="radio-inline">
+								<input type="radio" id="inlineCheckbox1" name="type" v-model="qhType" value="急功近利型">急功近利型
+							</label>
+							<label class="radio-inline">
+								<input type="radio" id="inlineCheckbox2" name="type" v-model="qhType" value="传统销售型">传统销售型
+							</label>
+							<label class="radio-inline">
+								<input type="radio" id="inlineCheckbox3" name="type" v-model="qhType" value="长远规划型">长远规划型
+							</label>
+							<label class="radio-inline">
+								<input type="radio" id="inlineCheckbox3" name="type" v-model="qhType" value="行业新鲜人">行业新鲜人
+							</label>
+						</td>
+					</tr>
+					<tr>
+						<td>公司地址</td>
+						<td colspan="5">
+							<div class="form-group col-sm-10">
+								<input type="text" class="form-control" v-model="address" id="address" required>
+							</div>
+						</td>
+					</tr>
+					<tr>
+						<td>注册教室时间</td>
+						<td colspan="2">
+							<div class="form-group">
+								<input type="text" class="form-control" v-model="registerTime" id="registerTime" required>
+							</div>
+						</td>
+						<td>推荐码</td>
+						<td colspan="2">
+							<div class="form-group">
+								<input type="text" class="form-control" v-model="registerNum" id="registerNum" required>
+							</div>
+						</td>
+					</tr>
+					<tr>
+						<td rowspan="4">客户主要负责人</td>
+						<td>姓名</td>
+						<td>职位</td>
+						<td>手机</td>
+						<td>年龄</td>
+						<td>性格描述</td>
+					</tr>
+					<tr>
+						<td>
+							<div class="form-group">
+
+								<input type="text" class="form-control" v-model="admin1Name" id="admin1Name" required>
+							</div>
+						</td>
+						<td>
+							<div class="form-group">
+
+								<input type="text" class="form-control" v-model="admin1Work" id="admin1Work" required>
+							</div>
+						</td>
+						<td>
+							<div class="form-group">
+
+								<input type="text" class="form-control" v-model="admin1Num" id="admin1Num" required>
+							</div>
+						</td>
+						<td>
+							<div class="form-group">
+
+								<input type="text" class="form-control" v-model="admin1Age" id="admin1Age" required>
+							</div>
+						</td>
+						<td>
+							<div class="form-group">
+
+								<input type="text" class="form-control" v-model="admin1Type" id="admin1Type" required>
+							</div>
+						</td>
+					</tr>
+					<tr>
+						<td>
+							<div class="form-group">
+
+								<input type="text" class="form-control" v-model="admin2Name" id="admin2Name" >
+							</div>
+						</td>
+						<td>
+							<div class="form-group">
+
+								<input type="text" class="form-control" v-model="admin2Work" id="admin2Work" >
+							</div>
+						</td>
+						<td>
+							<div class="form-group">
+
+								<input type="text" class="form-control" v-model="admin2Num" id="admin2Num" >
+							</div>
+						</td>
+						<td>
+							<div class="form-group">
+
+								<input type="text" class="form-control" v-model="admin2Age" id="admin2Age" >
+							</div>
+						</td>
+						<td>
+							<div class="form-group">
+
+								<input type="text" class="form-control" v-model="admin2Type" id="admin2Type" >
+							</div>
+						</td>
+					</tr>
+					<tr>
+						<td>
+							<div class="form-group">
+
+								<input type="text" class="form-control" v-model="admin3Name" id="admin3Name" >
+							</div>
+						</td>
+						<td>
+							<div class="form-group">
+
+								<input type="text" class="form-control" v-model="admin3Work" id="admin3Work" >
+							</div>
+						</td>
+						<td>
+							<div class="form-group">
+
+								<input type="text" class="form-control" v-model="admin3Num" id="admin3Num" >
+							</div>
+						</td>
+						<td>
+							<div class="form-group">
+
+								<input type="text" class="form-control" v-model="admin3Age" id="admin3Age" >
+							</div>
+						</td>
+						<td>
+							<div class="form-group">
+
+								<input type="text" class="form-control" v-model="admin3Type" id="admin3Type" required>
+							</div>
+						</td>
+					</tr>
+				</table>
+
+		</div>
+
+		<div class="table-warp customer-statu table-responsive">
+			<h2>二.客户状况</h2>
+			<table class="table table-bordered">
+				<tr>
+					<td>开业年数</td>
+					<td >
+						<div class="form-group">
+
+							<input type="text" class="form-control" v-model="qhAge" id="qhAge" required>
+						</div>
+					</td>
+					<td>地理位置</td>
+					<td >
+						<label class="radio-inline">
+							<input type="radio" id="inlineCheckbox1" name="qhArea" v-model="qhArea" value="商业区">商业区
+						</label>
+						<label class="radio-inline">
+							<input type="radio" id="inlineCheckbox2" name="qhArea" v-model="qhArea" value="商场">商场
+						</label>
+						<label class="radio-inline">
+							<input type="radio" id="inlineCheckbox3" name="qhArea" v-model="qhArea" value="社区">社区
+						</label>
+					</td>
+				</tr>
+				<tr>
+					<td>店面数量</td>
+					<td>
+						<div class="form-group">
+
+							<input type="text" class="form-control" v-model="qhNums" id="qhNums" required>
+						</div>
+					</td>
+					<td>琴房总数</td>
+					<td >
+						<div class="form-group">
+
+							<input type="text" class="form-control" v-model="qfNums" id="qfNums" required>
+						</div>
+					</td>
+				</tr>
+				<tr>
+					<td rowspan="5">主要品牌</td>
+					<td>产品</td>
+					<td>品牌</td>
+					<td>年销量</td>
+				</tr>
+				<tr>
+					<td>
+						<div class="form-group">
+
+							<input type="text" class="form-control" v-model="product1" id="product1" required>
+						</div>
+					</td>
+					<td>
+						<div class="form-group">
+
+							<input type="text" class="form-control" v-model="brand1" id="brand1" required>
+						</div>
+					</td>
+					<td>
+						<div class="form-group">
+
+							<input type="text" class="form-control" v-model="sales1" id="sales1" required>
+						</div>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<div class="form-group">
+
+							<input type="text" class="form-control" v-model="product2" id="product2">
+						</div>
+					</td>
+					<td>
+						<div class="form-group">
+
+							<input type="text" class="form-control" v-model="brand2" id="brand2">
+						</div>
+					</td>
+					<td>
+						<div class="form-group">
+
+							<input type="text" class="form-control" v-model="sales2" id="sales2">
+						</div>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<div class="form-group">
+
+							<input type="text" class="form-control" v-model="product3" id="product3">
+						</div>
+					</td>
+					<td>
+						<div class="form-group">
+
+							<input type="text" class="form-control" v-model="brand3" id="brand3">
+						</div>
+					</td>
+					<td>
+						<div class="form-group">
+
+							<input type="text" class="form-control" v-model="sales3" id="sales3">
+						</div>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<div class="form-group">
+
+							<input type="text" class="form-control" v-model="product4" id="product4">
+						</div>
+					</td>
+					<td>
+						<div class="form-group">
+
+							<input type="text" class="form-control" v-model="brand4" id="brand4">
+						</div>
+					</td>
+					<td>
+						<div class="form-group">
+
+							<input type="text" class="form-control" v-model="sales4" id="sales4">
+						</div>
+					</td>
+				</tr>
+			</table>
+
+			<button type="submit" class="btn btn-primary btn-lg btn-block">确认并提交</button>
+		</div>
+
+
+
+	</form>
+</div>
+</body>
+<script>
+	/**
+	 * 采用VUE进行数据绑定
+	 * 数据采集时,把form表单内部的input的name对应数据表字段即可
+	 * 数据调用时,php生成数据放置于data内部相应的键值对的键后面即可
+	 * @type {Vue}
+	 */
+	var app = new Vue({
+		el: '#qhMsg',
+		data: {
+			addresShi: '上海市',                       //琴行基本信息XX市
+			addresQu: '松江区',                        //琴行基本信息XX区
+			qhTitle: '佳音琴行',                          //琴行基本信息琴行名称
+			qhType:'急功近利型',                         //琴行类型
+			adminName:'张三',                         //客户名称
+			phoneNumber:'021-5888888',                  //公司电话
+			address:'上海市松江区XXXXX',                  //公司地址
+			registerTime:'2016-10-10',                  //注册教室时间
+			registerNum:'5235448522',                   //推荐码
+			admin1Name:'负责人1',                          //主要负责人姓名1
+			admin1Work:'经理',                            //主要负责人职位1
+			admin1Num:'158XXXXX',                       //主要负责人手机号1
+			admin1Age:'28',                             //主要负责人年龄1
+			admin1Type:'稳重',                            //主要负责人性格1
+			admin2Name:'负责人2',                          //主要负责人姓名2
+			admin2Work:'经理',                            //主要负责人职位2
+			admin2Num:'158XXXXX',                       //主要负责人手机号2
+			admin2Age:'38',                             //主要负责人年龄2
+			admin2Type:'稳重',                            //主要负责人性格2
+			admin3Name:'负责人3',                          //主要负责人姓名3
+			admin3Work:'经理',                            //主要负责人职位3
+			admin3Num:'158XXXXX',                           //主要负责人手机号3
+			admin3Age:'48',                                 //主要负责人年龄3
+			admin3Type:'稳重',                            //主要负责人性格3
+			qhAge:'10',                                 //琴行开业年数
+			qhArea:'社区',                                //琴行地理位置
+			qhNums:'1',                                 //琴行店面数量
+			qfNums:'10',                                //琴行琴房数量
+			product1:'钢琴',                              //主要品牌产品1
+			brand1:'法兰山德',                          //主要品牌1
+			sales1:'50台',                               //年销量1
+			product2:'提琴',                              //主要品牌产品2
+			brand2:'雅马哈',                           //主要品牌2
+			sales2:'50台',                               //年销量2
+			product3:'吉他',                              //主要品牌产品3
+			brand3:'法兰山德',                          //主要品牌3
+			sales3:'50台',                               //年销量3
+			product4:'钢琴',                              //主要品牌产品4
+			brand4:'法兰山德',                          //主要品牌4
+			sales4:'50台'                                //年销量4
+
+		}
+	})
+</script>
+</html>
