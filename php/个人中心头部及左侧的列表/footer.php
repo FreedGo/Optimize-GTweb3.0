8bd230d0e26648b09396e83ff24230fadc0b8758
Freed <flyxz@126.com>
2016-11-28 09:50:31 +0800

    个人中心头部及左侧,尾部备份

---
+++ php/个人中心头部及左侧的列表/footer.php
@@ -0,0 +1,142 @@
+<?php /*?><?php
+if(!defined('InEmpireCMS'))
+{
+	exit();
+}
+?><?php */?>
+</div>
+    </div>
+	<div class="footerWrap">
+		<div class="footBian">
+			<div class="footNavWrap clearfix">
+				<div class="footNav clearfix">
+					<dl>
+						<dd><a href="/guangchang">音乐广场</a></dd>
+					</dl>
+					<dl>
+						<dd><a href="/news">音乐资讯</a></dd>
+						<dt><a href="/news/yinyue">音乐新闻</a></dt>
+						<dt><a href="/news/yueqi">音乐资讯</a></dt>
+						<dt><a href="/news/huodong">音乐活动</a></dt>
+					</dl>
+					<!--<dl>
+						<dd><a href="/mingren">音乐名人</a></dd>
+						<dt><a href="/mingren">名人名家</a></dt>
+						<dt><a href="/mingren">音乐之星</a></dt>
+						
+					</dl>-->
+					<dl>
+						<dd><a href="/haixuan">网络海选</a></dd>
+						<dt><a href="/haixuan">中华好琴声</a></dt>
+						<dt><a href="/haixuan">城市音乐会</a></dt>
+						<dt><a href="/haixuan">两岸大师班</a></dt>
+					</dl>
+					<dl>
+						<dd><a href="/laoshi">音乐老师</a></dd>
+						<dt><a href="/laoshi">键盘老师</a></dt>
+						<dt><a href="/laoshi">吉他老师</a></dt>
+						<dt><a href="/laoshi">弦乐老师</a></dt>
+						<dt><a href="/laoshi">管乐老师</a></dt>
+						<dt><a href="/laoshi">民乐老师</a></dt>
+						<dt><a href="/laoshi">电声老师</a></dt>
+						<dt><a href="/laoshi">民乐老师</a></dt>
+						
+					</dl>
+					<dl>
+						<dd><a href="/jiaoshi">音乐教室</a></dd>
+					</dl>
+					<!-- <dl>
+						<dd><a href="/">音乐乐团</a></dd>
+						<dt><a href="/">管乐</a></dt>
+						<dt><a href="/">弦乐</a></dt>
+						<dt><a href="/">交响乐</a></dt>
+					                        <dt><a href="/">民乐</a></dt>
+					                        <dt><a href="/">合唱</a></dt>
+					</dl> -->
+					
+					<dl>
+						<dd><a href="/zhibo">直播课堂</a></dd>
+					</dl>
+					<dl>
+						<dd><a href="/yuepu">音乐乐谱</a></dd>
+						<dt><a href="/">钢琴</a></dt>
+						<dt><a href="/">提琴</a></dt>
+						<dt><a href="/">吉他</a></dt>
+					</dl>
+					
+					<!-- <dl>
+						<dd><a href="/">音乐百科</a></dd>
+						
+					</dl> -->
+					<!-- <dl>
+						<dd><a href="/">音乐电台</a></dd>
+						
+					</dl> -->
+					<dl>
+						<dd><a href="/bbs">声粉论坛</a></dd>
+						<dd><a href="/">乐迷互动</a></dd>
+						<dt><a href="/">学习交流</a></dt>
+						<dt><a href="/">乐器维修</a></dt>
+						<dt><a href="/">网站建议</a></dt>
+					</dl>
+                    
+                     <dl>
+						<dd><a>网站服务</a></dd>
+						<dt><a href="/t/guanggao/html/index-ad.html">广告服务</a></dt>
+                        <dt><a href="/">联系我们</a></dt>
+                        <dt><a href="/">关于我们</a></dt>
+                        <dt><a href="/">CEO专栏</a></dt>
+                        <dt><a href="/">版权声明</a></dt>
+                        
+					</dl>
+				</div>
+				<div class="footCode clearfix">
+					<div class="firstCode">
+						<a href="javascript:;">
+							<img src="/images/foot_weixin.jpg" alt="">
+							<h3>扫一扫关注微信</h3>
+						</a>
+					</div>
+					<div class="secondCode">
+						<a href="javascript:;">
+							<img src="/images/foot_app (1).jpg" alt="">
+							<h3>扫一扫下载APP</h3>
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="footer11">
+			<div class="footBtm clearfix">
+				<div class="h1">
+					<a href="javascript:;">
+						<h1>好琴声</h1>
+					</a>
+				</div>
+				<div class="lianxi">
+					<div class="footTil">
+						<span>好琴声</span>
+						<i>0 2 1 - 6 7 7 0 3 9 5 6</i>
+<!--						<p class="zhanzhang"><script type="text/javascript">var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_1256607007'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s4.cnzz.com/z_stat.php%3Fid%3D1256607007%26show%3Dpic' type='text/javascript'%3E%3C/script%3E"));</script></p>-->
+					</div>
+					<div class="footcon">
+						<p>Copyright © 2015 greattone.net All rights reserved. 好琴声版权所有</p>
+						<p>沪ICP备09050100号</p>
+						<p>公司总部地址：上海市金山区枫泾镇枫湾路531号科创小镇二楼 24小时服务热线：15216849061</p>
+						<p>地址：新竹市四維路80號  電話：03-528-5680 E-mail：chengyun0423@franzsandner.com</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
+<script>
+	var _hmt = _hmt || [];
+	(function() {
+		var hm = document.createElement("script");
+		hm.src = "//hm.baidu.com/hm.js?9e04abd1caf883d74ef53e2e445ddfe5";
+		var s = document.getElementsByTagName("script")[0];
+		s.parentNode.insertBefore(hm, s);
+	})();
+</script>
